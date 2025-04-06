c4fe271c0faabb6a54e1b91ea66328f3bd1b03e4
李昊壅 <cior1888@gmail.com>
2025-04-06 15:23:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -69,3 +69,7 @@ async function callGPT(userInput) {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
